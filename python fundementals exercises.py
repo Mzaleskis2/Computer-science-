7a55ded64aa780bcb71b0ac1d9b6c9b95235c108
Mzaleskis2 <20Mzaleskis.ACC@lwetb.ie>
2024-10-16 11:09:43 +0100

    Add files via upload

---
+++ python fundementals exercises.py
@@ -0,0 +1,44 @@
+#1
+pi = 3.14
+radius = float(input("What is the radius of a circle?"))
+area = pi*radius*radius
+print(area)
+#2
+cm = float(input('what is your height in centimeters?'))
+height_inches = cm / 2.54
+height_feet = height_inches / 12
+print('Your height is:', height_feet, 'feet and', height_inches, 'inches')
+#3
+n = int(input('Enter a number: '))
+a = n**2
+b = n**3
+c = n**4
+print(a)
+print(b)
+print(c)
+#4
+base = float(input('Enter the base: '))
+height = float(input('Enter the height: '))
+print(base/2*height)
+#5
+name = input("Enter your name")
+clas = input("Enter your class")
+age = int(input("Enter your age"))
+print(name, clas, age)
+print(name)
+print(clas)
+print(age)
+#6
+n = int(input("Enter a digit"))
+print(n,n+1,n+2)
+### im not sure how to put these together
+#7
+digit = int(input("enter a number"))
+digit2 = int(input("enter a number"))
+digit3 = int(input("enter a number"))
+digit4 = (digit + digit2)
+digit5 = (digit2 + digit3)
+print(digit, digit2, digit3,)
+print(digit4, digit5, digit3,)
+
+
